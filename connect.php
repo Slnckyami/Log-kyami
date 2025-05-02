1c7935a7fb60982df8f2fe2ab0fd6d2a62b7df76
Kyami Silence <116436066+Slnckyami@users.noreply.github.com>
2025-05-02 16:46:18 +0800

    Add files via upload

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if($username == 'admin' && $password == '12345'){
-    header('Location: index.html');
+    header('Location: connecta/index.html ');
     exit();
 } else {
     echo "Username atau password salah";
